44b7c039a2285ebbabcd53d0bf0cf0a7ed5c5058
Tuanemendes <tuane.belmiro@gmail.com>
2023-02-14 22:26:18 -0300

    Initial commit

---
+++ src/main/java/one/digitalinnovation/labpadraoprojetospring/LabPadraoProjetoSpringApplication.java
@@ -0,0 +1,13 @@
+package one.digitalinnovation.labpadraoprojetospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabPadraoProjetoSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabPadraoProjetoSpringApplication.class, args);
+	}
+
+}
